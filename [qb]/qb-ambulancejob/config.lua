3a5a94a3ae6f9ed46ba63cac9612da28626981e8
Chill <curtis.hill1@gmail.com>
2022-05-12 15:28:39 -0700

    updated ambulancejob to sandy hospital

--- [qb]/qb-ambulancejob/config.lua
+++ [qb]/qb-ambulancejob/config.lua
@@ -7,50 +7,36 @@ Config.WipeInventoryOnRespawn = true
 
 Config.Locations = {
     ["checking"] = {
-	    [1] = vector3(308.19, -595.35, 43.29),
-	    [2] = vector3(-254.54, 6331.78, 32.43), -- paleto
+	    [1] = vector3(1828.2, 3685.29, 34.27),
     },
     ["duty"] = {
-        [1] = vector3(311.18, -599.25, 43.29),
-        [2] = vector3(-254.88, 6324.5, 32.58),
+        [1] = vector3(1826.23, 3685.75, 34.27),
     },
     ["vehicle"] = {
-        [1] = vector4(294.578, -574.761, 43.179, 35.79),
-        [2] = vector4(-234.28, 6329.16, 32.15, 222.5),
+        [1] = vector4(1811.28, 3684.75, 34.22, 294.63),
     },
     ["helicopter"] = {
         [1] = vector4(351.58, -587.45, 74.16, 160.5),
         [2] = vector4(-475.43, 5988.353, 31.716, 31.34),
     },
     ["armory"] = {
-        [1] = vector3(309.93, -602.94, 43.29),
-        [2] = vector3(-245.13, 6315.71, 32.82),
+        [1] = vector3(1823.12, 3666.61, 34.27),
     },
     ["roof"] = {
         [1] = vector4(338.5, -583.85, 74.16, 245.5),
     },
     ["main"] = {
-        [1] = vector3(298.74, -599.33, 43.29),
-    },
-    ["stash"] = {
         [1] = vector3(309.78, -596.6, 43.29),
     },
+    ["stash"] = {
+        [1] = vector3(1830.51, 3670.49, 34.27),
+    },
     ["beds"] = {
-        [1] = {coords = vector4(353.1, -584.6, 43.11, 152.08), taken = false, model = 1631638868},
-        [2] = {coords = vector4(356.79, -585.86, 43.11, 152.08), taken = false, model = 1631638868},
-        [3] = {coords = vector4(354.12, -593.12, 43.1, 336.32), taken = false, model = 2117668672},
-        [4] = {coords = vector4(350.79, -591.8, 43.1, 336.32), taken = false, model = 2117668672},
-        [5] = {coords = vector4(346.99, -590.48, 43.1, 336.32), taken = false, model = 2117668672},
-        [6] = {coords = vector4(360.32, -587.19, 43.02, 152.08), taken = false, model = -1091386327},
-        [7] = {coords = vector4(349.82, -583.33, 43.02, 152.08), taken = false, model = -1091386327},
-        [8] = {coords = vector4(326.98, -576.17, 43.02, 152.08), taken = false, model = -1091386327},
-	--- paleto
-	    [9] = {coords = vector4(-252.43, 6312.25, 32.34, 313.48), taken = false, model = 2117668672},
-        [10] = {coords = vector4(-247.04, 6317.95, 32.34, 134.64), taken = false, model = 2117668672},
-        [11] = {coords = vector4(-255.98, 6315.67, 32.34, 313.91), taken = false, model = 2117668672},
+        [1] = {coords = vector4(1826.44, 3676.7, 35.2, 26.05), taken = false, model = 1631638868},
+        [2] = {coords = vector4(1819.77, 3672.76, 35.2, 358.75), taken = false, model = 1631638868},
     },
     ["stations"] = {
-        [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(304.27, -600.33, 43.28, 272.249)}
+        [1] = {label = Lang:t('info.pb_hospital'), coords = vector4(1828.2, 3685.29, 34.27, 116.83)}
     }
 }
 
@@ -135,9 +121,9 @@ Config.Items = {
     }
 }
 
-Config.BillCost = 2000
+Config.BillCost = 1000
 Config.DeathTime = 300
-Config.CheckTime = 10
+Config.CheckTime = 5
 
 Config.PainkillerInterval = 60 -- seconds
 
