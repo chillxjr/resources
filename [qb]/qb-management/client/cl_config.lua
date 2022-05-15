caf9a6b34357a80e6b9159f4d28452c97735f7d3
Chill <curtis.hill1@gmail.com>
2022-05-14 18:21:53 -0700

    fixed boss menu locations

--- [qb]/qb-management/client/cl_config.lua
+++ [qb]/qb-management/client/cl_config.lua
@@ -5,10 +5,10 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 
 Config.BossMenus = {
     ['police'] = {
-        vector3(461.45, -986.2, 30.73),
+        vector3(1862.31, 3691.46, 34.26),
     },
     ['ambulance'] = {
-        vector3(335.46, -594.52, 43.28),
+        vector3(1836.05, 3691.46, 34.27),
     },
     ['realestate'] = {
         vector3(-716.11, 261.21, 84.14),
@@ -20,16 +20,16 @@ Config.BossMenus = {
         vector3(-27.47, -1107.13, 27.27),
     },
     ['mechanic'] = {
-        vector3(-339.53, -156.44, 44.59),
+        vector3(1186.42, 2636.49, 38.4),
     },
 }
 
 Config.BossMenuZones = {
     ['police'] = {
-        { coords = vector3(461.45, -986.2, 30.73), length = 0.35, width = 0.45, heading = 351.0, minZ = 30.58, maxZ = 30.68 } ,
+        { coords = vector3(1862.31, 3691.46, 34.26), length = 0.35, width = 0.45, heading = 351.0, minZ = 30.58, maxZ = 30.68 } ,
     },
     ['ambulance'] = {
-        { coords = vector3(335.46, -594.52, 43.28), length = 1.2, width = 0.6, heading = 341.0, minZ = 43.13, maxZ = 43.73 },
+        { coords = vector3(1836.05, 3691.46, 34.27), length = 1.2, width = 0.6, heading = 341.0, minZ = 43.13, maxZ = 43.73 },
     },
     ['realestate'] = {
         { coords = vector3(-716.11, 261.21, 84.14), length = 0.6, width = 1.0, heading = 25.0, minZ = 83.943, maxZ = 84.74 },
@@ -41,7 +41,7 @@ Config.BossMenuZones = {
         { coords = vector3(-27.47, -1107.13, 27.27), length = 2.4, width = 1.05, heading = 340.0, minZ = 27.07, maxZ = 27.67 },
     },
     ['mechanic'] = {
-        { coords = vector3(-339.53, -156.44, 44.59), length = 1.15, width = 2.6, heading = 353.0, minZ = 43.59, maxZ = 44.99 },
+        { coords = vector3(1186.42, 2636.49, 38.4), length = 1.15, width = 2.6, heading = 353.0, minZ = 43.59, maxZ = 44.99 },
     },
 }
 
