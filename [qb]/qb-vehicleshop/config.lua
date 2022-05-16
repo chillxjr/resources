5bf9c6a07b29cc51d65b612268a34e4fdb379007
Chill <curtis.hill1@gmail.com>
2022-05-16 02:20:43 -0700

    fixed veh shop name

--- [qb]/qb-vehicleshop/config.lua
+++ [qb]/qb-vehicleshop/config.lua
@@ -22,7 +22,7 @@ Config.Shops = {
             ['size'] = 2.75, -- size of the vehicles zones
         },
         ['Job'] = 'none', -- Name of job or none
-        ['ShopLabel'] = 'Premium Deluxe Motorsport', -- Blip name
+        ['ShopLabel'] = 'Derricks Deals', -- Blip name
         ['showBlip'] = true,  -- true or false
         ['blipSprite'] = 326,  -- Blip sprite
         ['blipColor'] = 3,  -- Blip color
