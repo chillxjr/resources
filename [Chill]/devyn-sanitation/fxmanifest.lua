344957ab1381ce8ee74639ecbf7f474dc39da37c
Chill <curtis.hill1@gmail.com>
2022-05-16 18:51:10 -0700

    added player groups and garbage job

---
+++ [Chill]/devyn-sanitation/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'devyn'
+
+client_scripts {"client/*.lua"}
+server_scripts {"server/*.lua"}
+
+shared_scripts {'shared/*.lua'}
+lua54 'yes'
---
+++ [Chill]/devyn-sanitation/shared/config.lua
@@ -0,0 +1,78 @@
+Config = {}
+
+Config.NPC = vector4(2041.33, 3187.38, 45.18, 247.76)
+
+Config.Blip = vector3(2043.56, 3180.58, 44.97)
+
+Config.TruckSpawns = {
+    vector4(2043.56, 3180.58, 44.97, 150.74),
+}
+
+Config.Rewards = {
+    {
+        item  = "plastic",
+        min = 2,
+        max = 5,
+    },
+    {
+        item  = "rubber",
+        min = 2,
+        max = 5,
+    },
+    {
+        item  = "metalscrap",
+        min = 2,
+        max = 5,
+    },
+    {
+        item  = "steel",
+        min = 2,
+        max = 5,
+    },
+    {
+        item  = "meth",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "cokebaggy",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "weed_skunk",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "lockpick",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "iphone",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "samsungphone",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "laptop",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "tablet",
+        min = 0,
+        max = 1,
+    },
+    {
+        item  = "cryptostick",
+        min = 0,
+        max = 1,
+    },
+    
+}
