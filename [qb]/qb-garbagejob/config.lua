71d5a04fd61a417687f71ae64dc80913aa04c72b
Pouncer012 <32249102+Pouncer012@users.noreply.github.com>
2022-05-23 16:27:33 -0500

    Updated Garbage Job Location

--- [qb]/qb-garbagejob/config.lua
+++ [qb]/qb-garbagejob/config.lua
@@ -33,7 +33,7 @@ Config.UsePreconfiguredRoutes = false
 Config.Peds = {
     {
         model = 's_m_y_garbage',
-        coords = vector4(-321.87, -1544.77, 31.02, 289.6),
+        coords = vector4(-321.87, -1545.77, 30.02, 289.6),
         zoneOptions = { -- Used for when UseTarget is false
             length = 3.0,
             width = 3.0
