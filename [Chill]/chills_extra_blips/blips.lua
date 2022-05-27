3852a4355b4c17277156e4547af5f31c7a34fcdc
Chill <curtis.hill1@gmail.com>
2022-05-26 20:48:41 -0700

    added gardening/blip

--- [Chill]/chills_extra_blips/blips.lua
+++ [Chill]/chills_extra_blips/blips.lua
@@ -3,7 +3,8 @@ local blips = {
 
      {title="Vineyard", colour=27, id=408, x = -1886.57, y = 2049.96, z = 140.98}, --grapesvector3(-1886.57, 2049.96, 140.98)
      {title="Make Grape Juice", colour=27, id=604, x = -1923.82, y = 2054.44, z = 140.83}, --juice
-     {title="Make Wine", colour=27, id=93, x = -1926.96, y = 2042.03, z = 140.83} --wine
+     {title="Make Wine", colour=27, id=93, x = -1926.96, y = 2042.03, z = 140.83}, --wine vector3(-540.65, -205.78, 37.65)
+     {title="Gardening", colour=2, id=280, x = -540.65, y = -205.78, z = 37.65} --wine vector3(-540.65, -205.78, 37.65)
   }
       
 Citizen.CreateThread(function()
---
+++ [Jobs]/gardening/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'gardening'
+
+shared_script 'config.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+dependencies {
+    'qb-core',
+    'PolyZone',
+    'qb-target'
+}
