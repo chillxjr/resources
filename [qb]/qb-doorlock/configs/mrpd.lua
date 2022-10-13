144dcac8a99169b6adc759f5439239d2707f4b77
Chill <curtis.hill1@gmail.com>
2022-10-12 17:44:51 -0700

    added mission row PD

---
+++ [qb]/qb-doorlock/configs/mrpd.lua
@@ -0,0 +1,225 @@
+
+
+-- entry_1 created by Chill Hillington
+Config.DoorList['mrpd-entry_1'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Entry',
+    doors = {
+        {objName = 952639784, objYaw = 89.999977111816, objCoords = vec3(449.522369, -982.958435, 30.874449)},
+        {objName = -1481015543, objYaw = 89.999977111816, objCoords = vec3(449.522369, -980.361023, 30.874449)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
+
+-- entry_2 created by Chill Hillington
+Config.DoorList['mrpd-entry_2'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Entry',
+    doors = {
+        {objName = -688443112, objYaw = 0.0, objCoords = vec3(440.114258, -998.584351, 31.118141)},
+        {objName = 1847320387, objYaw = 360.0, objCoords = vec3(442.750885, -998.583801, 31.118603)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
+
+-- press_room created by Chill Hillington
+Config.DoorList['mrpd-press_room'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Press Room',
+    doors = {
+        {objName = -1481015543, objYaw = 179.99998474121, objCoords = vec3(434.526550, -989.491760, 30.873428)},
+        {objName = 952639784, objYaw = 180.00001525879, objCoords = vec3(437.124390, -989.491760, 30.873428)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
+
+-- armory created by Chill Hillington
+Config.DoorList['mrpd-armory'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 315.0,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Armory',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(458.398224, -992.107056, 35.212627),
+}
+
+-- evidence created by Chill Hillington
+Config.DoorList['mrpd-evidence'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999977111816,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = 749848321,
+    doorLabel = 'Evidence',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(468.018616, -998.500244, 30.611513),
+}
+
+-- jail_1 created by Chill Hillington
+Config.DoorList['mrpd-jail_1'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(484.083130, -1006.736145, 30.612476),
+}
+
+-- jail_2 created by Chill Hillington
+Config.DoorList['mrpd-jail_2'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(484.083069, -1010.842529, 30.612476),
+}
+
+-- jail_3 created by Chill Hillington
+Config.DoorList['mrpd-jail_3'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(484.083069, -1014.943298, 30.612476),
+}
+
+-- jail_4 created by Chill Hillington
+Config.DoorList['mrpd-jail_4'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(480.817352, -1006.746033, 30.612476),
+}
+
+-- jail_5 created by Chill Hillington
+Config.DoorList['mrpd-jail_5'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(480.817291, -1010.842468, 30.612476),
+}
+
+-- jail_6 created by Chill Hillington
+Config.DoorList['mrpd-jail_6'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    objYaw = 89.999961853027,
+    pickable = true,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objName = -1036090959,
+    doorLabel = 'Jail Door',
+    locked = true,
+    distance = 2,
+    doorType = 'door',
+    objCoords = vec3(480.817291, -1014.956787, 30.612476),
+}
+
+-- back_entrance1 created by Chill Hillington
+Config.DoorList['mrpd-back_entrance1'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Back Entrance',
+    doors = {
+        {objName = -1036090959, objYaw = 0.0, objCoords = vec3(477.091370, -979.453003, 28.143826)},
+        {objName = -1036090959, objYaw = 180.00001525879, objCoords = vec3(479.690887, -979.453003, 28.143826)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
+
+-- garage_entrance created by Chill Hillington
+Config.DoorList['mrpd-garage_entrance'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Garage Entrance',
+    doors = {
+        {objName = 1255964982, objYaw = 270.00003051758, objCoords = vec3(463.187622, -991.515686, 25.617481)},
+        {objName = 1255964982, objYaw = 90.000022888184, objCoords = vec3(463.187622, -988.916992, 25.617481)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
+
+-- garage_entrance2 created by Chill Hillington
+Config.DoorList['mrpd-garage_entrance2'] = {
+    doorRate = 1.0,
+    hideLabel = true,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Garage Entrance',
+    doors = {
+        {objName = 1255964982, objYaw = 1.0017911336035e-05, objCoords = vec3(474.457062, -993.203369, 25.616543)},
+        {objName = 1255964982, objYaw = 180.00001525879, objCoords = vec3(471.858215, -993.203369, 25.616543)}
+    },
+    distance = 2,
+    doorType = 'double',
+    pickable = true,
+}
